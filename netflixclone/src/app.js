a088822569b1e613c54d3c609e313da75a7c9a0e
DylanEngle <104050134+DylanEngle@users.noreply.github.com>
2024-08-31 20:46:33 -0400

    add opt in component

--- netflixclone/src/app.js
+++ netflixclone/src/app.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import {BrowserRouter as Router, Route,  Switch} from 'react-router-dom';
 import { JumbotronContainer } from './containers/jumbotron';
 import { FooterContainer } from './containers/footer';
 import { FaqsContainer } from './containers/faqs';
+import * as ROUTES from './constants/routes';
 
 function App() {
   return (
     <>
-      <JumbotronContainer/>
-      <FaqsContainer />
-      <FooterContainer />
-      
+        <JumbotronContainer/>
+        <FaqsContainer />
+        <FooterContainer />
     </>
   );
 }
--- netflixclone/src/containers/faqs.js
+++ netflixclone/src/containers/faqs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Accordion} from '../components';
 import faqsData from '../fixtures/faqs';
+import OptForm from '../components/opt-form';
 
 export function FaqsContainer(){
     return(
@@ -14,7 +15,12 @@ export function FaqsContainer(){
                 </Accordion.Item>
             ))}
             </Accordion.Frame>
-            
+            <OptForm>
+                <OptForm.Input placeholder='Email address'></OptForm.Input>
+                <OptForm.Button>Try it now</OptForm.Button>
+                <OptForm.Break></OptForm.Break>
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+            </OptForm>
         </Accordion>
     );
 }
